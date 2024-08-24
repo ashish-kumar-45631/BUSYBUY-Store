b077e1e4e0254bad5ea4fe7809b109d1efd50075
Ashish Kumar <143773138+Ashish-Kumar-45631@users.noreply.github.com>
2024-08-24 08:58:00 +0530

    Delete src directory

--- src/components/Product/ProductContainer/ProductContainer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styles from "./ProductContainer.module.css";
-
-// Product card container
-const ProductContainer = ({ children }) => {
-  return <div className={styles.productContainer}>{children}</div>;
-};
-
-export default ProductContainer;
--- src/pages/NotFoundPage/NotFoundPage.js
+++
@@ -1,9 +0,0 @@
-import styles from "./NotFound.module.css";
-
-export default function NotFoundPage() {
-  return (
-    <div className={styles.notFound}>
-      <h1>Page not found</h1>
-    </div>
-  );
-}
